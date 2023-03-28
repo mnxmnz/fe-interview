60b0683e09e02fe53a8bbbd44e204f7a131c3b8a
mnxmnz <minzidev@gmail.com>
2023-03-28 15:18:23 +0900

    :tada: Docusaurus 프로젝트 초기 설정

---
+++ docusaurus.config.js
@@ -0,0 +1,65 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Front-End Interview',
+  tagline: '신입 프론트엔드 개발자로 면접에 참여했을 때 받았던 질문을 정리했습니다.',
+  favicon: 'img/icon.png',
+  url: 'https://fe-interview-mnxmnz.vercel.app',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'ko',
+    locales: ['ko'],
+  },
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Front-End Interview',
+        logo: {
+          alt: 'Logo',
+          src: 'img/icon.png',
+        },
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/mnxmnz/FE-Interview',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} 김민지 Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
